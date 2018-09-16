56809a8674221db5ec2daa369c2075250772546c
patriciafelipe <patricia-felipe@hotmail.com>
2018-09-16 20:29:59 -0300

    Alterado

--- js/mascaravalidacao.js
+++ js/mascaravalidacao.js
@@ -1,7 +1,15 @@
-function mCPF(cpf){
-				cpf=cpf.replace(/\D/g,"")
-				cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
-				cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
-				cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
-				return cpf
+function mcpf(cpf){
+				cpf.value=cpf.value.replace(/\D/g,"")
+				cpf.value=cpf.value.replace(/(\d{3})(\d)/,"$1.$2")
+				cpf.value=cpf.value.replace(/(\d{3})(\d)/,"$1.$2")
+				cpf.value=cpf.value.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
 			}
+function mtel(v){
+    v.value=v.value.replace(/\D/g,"");            
+    v.value=v.value.replace(/^(\d{2})(\d)/g,"($1) $2"); 
+    v.value=v.value.replace(/(\d)(\d{3})$/,"$1-$2");   
+}
+function mcep(cep){
+    cep.value=cep.value.replace(/\D/g,"");            
+    cep.value=cep.value.replace(/(\d)(\d{3})$/,"$1-$2");
+}
